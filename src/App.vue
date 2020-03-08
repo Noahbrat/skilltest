8cb36f4e361cc2717dcac287be571fd31cedcc00
noahone <tech@noahbratzel.com>
2020-03-08 15:57:57 -0500

    Move getStories to be within vue and remove hello world stuff

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>WHCC Hacker News Reader</h1>
+    <stories></stories>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Stories from './components/Stories.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Stories
   }
 }
 </script>
---
+++ src/components/Stories.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="stories">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stories',
+  props: {
+  },
+  data: function () {
+      return {
+        storyIds: []
+      }
+  },
+  methods: {
+    getStories: function () {
+      fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
+          .then((response) => {
+              return response.json();
+          })
+          .then((myJson) => {
+              this.storyIds = myJson;
+          });
+      }
+  },
+  created: function () {
+    this.getStories();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
